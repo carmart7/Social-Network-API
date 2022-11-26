b3fc54ea2cc55bbb9f3170f194dde6d8f9421ddf
[carlos martinez] <theycallhellp@gmail.com>
2022-11-26 13:58:37 -0800

    connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mogodb://localhost:27017/socialNetworkDB', {
+mongoose.connect('mongodb://localhost:27017/socialNetworkDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const db = require('./config/connection');
+const { User, Thought } = require('./models')
 
 db.once('open', () => {
     // callback to run express app listen in the future
-})
+    console.log('working db opened');
+});
