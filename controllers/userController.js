5ab39eadf9e43f2ed64f03c46cbc4627e6c81e73
[carlos martinez] <theycallhellp@gmail.com>
2022-11-26 15:48:25 -0800

    delete user by id route deletes related thoughts

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,18 @@
 const { User, Thought } = require('../models');
 
+// function that deletes thoughts with the associated username
+function deleteRelatedThoughts (id) {
+    Thought.deleteMany({ _id: id});
+}
+
 module.exports = {
-    // Create a new user with request body data 
+    //  Create a new user with request body data 
     createUser(req, res) {
         User.create(req.body)
         .then((student) => res.json(student))
         .catch((err) => res.status(500).json(err));
     },
+    // Get all users
     getUsers(req, res) {
         User.find()
             .populate('thoughts')
@@ -14,6 +20,7 @@ module.exports = {
             .then((student) => res.json(student))
             .catch((err) => res.status(500).json(err));
     },
+    // Get specific User by Id
     getSpecificUser(req, res) {
         User.findOne({ _id: req.params.id})
             .populate('thoughts')
@@ -21,14 +28,19 @@ module.exports = {
             .then((student) => res.json(student))
             .catch((err) => res.status(500).json(err));
     },
+    // Update a User's username by Id
     updateUser(req, res) {
         User.updateOne({ _id: req.params.id }, { username: req.body.username })
             .then((student) => res.json(student))
             .catch((err) => res.status(500).json(err));
     },
+    // Delete a User by Id
     deleteUser(req, res) {
         User.deleteOne({ _id: req.params.id })
-            .then((student) => res.json(student))
+            .then((student) => {
+                deleteRelatedThoughts(req.params.id) //delete related thoughts
+                return res.json(student)
+            })
             .catch((err) => res.status(500).json(err));
     }
 };
