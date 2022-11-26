1e750a152634e9e2d03dec0fb7e570211be2ed13
[carlos martinez] <theycallhellp@gmail.com>
2022-11-26 13:44:08 -0800

    reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const reactionSchema = mongoose.Schema(
+    {
+        reactionId: {
+            type: mongoose.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formatDate
+        }
+    }
+)
+
+function formatDate(createdAt) {
+    return createdAt.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
+}
+
+module.exports = reactionSchema;
